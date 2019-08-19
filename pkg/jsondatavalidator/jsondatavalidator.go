f2ede2f82fe72fb05ec675528bb95cc08cd1745a
vishwanath jayaraman <vishwanathjayaraman@vishbook.attlocal.net>
2019-08-18 20:32:24 -0500

    removed adhoc test code

--- pkg/jsondatavalidator/jsondatavalidator.go
+++ pkg/jsondatavalidator/jsondatavalidator.go
@@ -48,13 +48,6 @@ func init() {
 	localUnitTest := os.Getenv("TEST")
 	log.Debug(localUnitTest)
 
-	SchemaDir = "../schema/"
-	SchemaInputPath = "../schema/vnfdInputSchema.json#/vnfdInput"
-	SchemaParameterizedInstanceRelPath = "../schema/vnfdInstanceSchema.json#/vnfdInstance"
-	SchemaPaginatedInstancesRelPath = "../schema/vnfdPaginatedInstanceSchema.json#/vnfdsPaginatedInstances"
-	SchemaFileInputParam = "inputParam.json"
-	SchemaFileDefineNonParam = "vnfdDefineNonParam.json"
-	/*
 	if localUnitTest == "true" {
 		SchemaDir = "../schema/"
 		SchemaInputPath = "../schema/vnfdInputSchema.json#/vnfdInput"
@@ -69,7 +62,7 @@ func init() {
 		SchemaPaginatedInstancesRelPath = "/usr/share/vnfdservice/schema/vnfdPaginatedInstanceSchema.json#/vnfdsPaginatedInstances"
 		SchemaFileInputParam = "inputParam.json"
 		SchemaFileDefineNonParam = "vnfdDefineNonParam.json"
-	}*/
+	}
 }
 
 const (
