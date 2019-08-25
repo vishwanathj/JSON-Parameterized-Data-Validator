b4c8b146b75e5c011a80ac1046206c5a2b0138b3
vishwanath jayaraman <vishwanathjayaraman@vishbook.attlocal.net>
2019-08-25 11:33:18 -0500

    removed extraneous files

--- test/postValidNonParameterizedVnfds.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST -i http://localhost:8080/vnfds --data  "@/usr/share/vnfdservice/test/yamlfiles/valid/nonParameterizedInput/validNonParameterizedVNFDInputWithOptionalPropConstraintsMissing.json"
